14b6eda26cdb7b61d8456b61b49e17d914a321c5
Ciaran Robertson <egycr1@nottingham.ac.uk>
2023-10-10 11:58:59 +0100

    char variable

--- C5/DisplayingVariables/displaying_variables.c
+++ C5/DisplayingVariables/displaying_variables.c
@@ -6,9 +6,10 @@ int main(void)
     // Declare some variables
     int a = 1, b = 2;
     float f = 1.23f;
+    char c = 'b';
 
     // Use printf display text on the screen
-    printf ("The variables are\na = %d\nb=%d\nf=%f", a, b, f);
+    printf ("The variables are\na = %d\nb=%d\nf=%f\nc=%c\n", a, b, f, c);
 
     // Exit from main
     return 0;
