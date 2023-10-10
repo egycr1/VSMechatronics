24ee27da60cf220ecb68ebe43af8a1e095dc1b8b
Ciaran Robertson <egycr1@nottingham.ac.uk>
2023-10-10 13:16:29 +0100

    Print variable assignments

--- C6/inc_dec_examples.c
+++ C6/inc_dec_examples.c
@@ -9,14 +9,22 @@ int main(void)
     // Note: Order of a,b indicates the order in which operations are carried out on execution
     
     // Increment operators
+    a = 0;
     b = 3;
+    printf("a=%d\nb=%d\n", a, b); // Print new values for a & b
     a = ++b;    // b is now 4, a is also 4
+    printf("a=%d\nb=%d\n", a, b); // Print new values for a & b
     a = b++;    // a is 4, b is now 5, 
+    printf("a=%d\nb=%d\n", a, b); // Print new values for a & b
     
-    // Decrement operators (reset a back to 3)
+    // Decrement operators (reset a & b)
+    a = 0;
     b = 3;
-    a = b--;    // a is 3, b is now 2    
+    printf("a=%d\nb=%d\n", a, b); // Print new values for a & b
+    a = b--;    // a is 3, b is now 2
+    printf("a=%d\nb=%d\n", a, b);    // Print new values for a & b
     a = --b;    // b is now 1, a is also 1
+    printf("a=%d\nb=%d\n", a, b); // Print new values for a & b
     
     return 0;	// Exit from main
 }
